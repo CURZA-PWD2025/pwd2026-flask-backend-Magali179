87e8cc757611bf876251cf5ed90617bbe16723c2
Magali179 <malali.moreno@gmail.com>
2026-04-19 13:46:21 -0300

    migraciones

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,10 @@ from dotenv import load_dotenv
 from flask import Flask
 from app.models import db
 from app.config import config
+from app.routes.categoria_routes import categoria_bp
+from app.routes.movimientos_routes import movimiento_bp
+from app.routes.producto_routes import producto_bp
+from app.routes.proveedor_routes import proveedor_bp
 from app.routes.user_routes import users
 from app.routes.rol_routes import roles
 from app.routes.auth_routes import auth_bp
@@ -20,17 +24,21 @@ def create_app():
     app.register_blueprint(users)
     app.register_blueprint(roles)
     app.register_blueprint(auth_bp)
-    
+    app.register_blueprint(categoria_bp)
+    app.register_blueprint(proveedor_bp)
+    app.register_blueprint(producto_bp)
+    app.register_blueprint(movimiento_bp)
+
     @app.route('/')
     @app.route('/<nombre>')    
     def home(nombre = None):
         if (nombre == None):
-            return f' <h1>Hola  desde programacion web dinamica 2026<h1>'
-        return f'Hola {nombre} te saludamos desde programacion web dinamica 2026'
+            return f' <h1>Hola  desde Patagones<h1>'
+        return f'Hola {nombre} te saludamos desde Patagones'
 
     @app.route('/saludo')
     def saludo():
-        return f'Hola desde programacion web dinamica 2026 saludo'
+        return f'Hola desde Patagones saludo'
     db.init_app(app)
     migrate.init_app(app=app, db=db)
     jwt.init_app(app)
--- backend/app/models/producto.py
+++ backend/app/models/producto.py
@@ -7,8 +7,8 @@ class Producto(BaseModel):
    
     nombre = db.Column(db.String(150),  nullable = False)
     descripcion = db.Column(db.Text, nullable = True)
-    precio_costo= db.Column(db.Numeric(10.2), nullable = False)
-    precio_venta = db.Column(db.Numeric(10.2), nullable = False)
+    precio_costo= db.Column(db.Numeric(10,2), nullable = False)
+    precio_venta = db.Column(db.Numeric(10,2), nullable = False)
     stock_actual = db.Column(db.Integer, default = 0)
     stock_minimo = db.Column(db.Integer, default = 0)
     categoria_id = db.Column(db.Integer, db.ForeignKey('categorias.id'), nullable = False)
@@ -20,7 +20,7 @@ class Producto(BaseModel):
     
     
     
-    def __init__(self, nombre, descripcion, precio_costo, precio_venta, stock_actual, stock_minimo,  categoria_id, proveedor_id) None:
+    def __init__(self, nombre, descripcion, precio_costo, precio_venta, stock_actual, stock_minimo,  categoria_id, proveedor_id):
         self.nombre = nombre
         self.descripcion = descripcion
         self.precio_costo = precio_costo
